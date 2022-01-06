75a83cb6fe36c99c703141257f06ae2ded235e50
shahud <shahudmalik5@gmail.com>
2022-01-06 22:12:09 +0500

    3 demos completed

---
+++ Scripts/script.js
@@ -0,0 +1,10 @@
+const containerDOM = document.getElementsByClassName('container')
+
+
+let ChangeDisplay = (changeSlider, hideSlider) => {
+    
+    containerDOM[changeSlider].classList.remove("container-hide")
+    containerDOM[hideSlider].classList.add("container-hide")
+
+}
+setTimeout(()=>{ChangeDisplay(1,0)}, 2000);
